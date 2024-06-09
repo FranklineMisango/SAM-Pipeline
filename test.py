3833c0609c4eb3360720c07748c0616e869e599b
misango <franklinemisango@gmail.com>
2024-06-09 21:43:03 +0800

    Cloud fetch the .txt file

--- test.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-from lang_sam import LangSAM
-
-model = LangSAM()
-image_pil = Image.open("Test.jpg").convert("RGB")
-text_prompt = "car"
-masks, boxes, phrases, logits = model.predict(image_pil, text_prompt)
